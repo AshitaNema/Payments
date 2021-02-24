cb4a83f0864de0c0cdd7e99a1a91f6ead64cbdd3
Ashita-09 <ashitanema2089@gmail.com>
2021-02-24 17:47:03 +0530

    initial

---
+++ example/lib/config.dart
@@ -0,0 +1,5 @@
+
+
+const String squareApplicationId = "sandbox-sq0idb-Mk6nsJqHRLCWveX19IYFKA";
+const String squareLocationId = "L769RGHMTAXVN";
+const String applePayMerchantId = "REPLACE_ME";
---
+++ lib/google_pay_constants.dart
@@ -0,0 +1,8 @@
+
+
+const int totalPriceStatusNotCurrentlyKnown = 1;
+const int totalPriceStatusEstimated = 2;
+const int totalPriceStatusFinal = 3;
+
+const int environmentProduction = 1;
+const int environmentTest = 3;
